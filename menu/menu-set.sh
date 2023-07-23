c096d1481b54378196313d28a6ce99efc7c49806
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-23 15:12:16 +0700

    menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -38,7 +38,7 @@ case $opt in
 8) clear ; menu-webmin ; exit ;; 
 9) clear ; about ; exit ;;
 10) clear ; up ; exit ;;
-0) clear ; up ; exit ;;
+0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "salah pencet asu" ; sleep 1 ; menu-set ;;
 esac
