dd568c0b662dd65b34ef3ff5b6240f7cffe60193
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-23 17:11:31 +0700

    menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -303,7 +303,7 @@ clear
 		    echo -e "╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛${NC}" | lolcat 
                     echo -e "${Kcyan}┌───────────────────────────────────────────┐${NC}" 
                     echo -e "${Kcyan}│${NC} ${white} IP VPS  ${NC}: $IPVPS"
-		    echo -e "${Kcyan}│${NC} ${white} Domain  ${NC}: $cat /etc/xray/domain "
+		    echo -e "${Kcyan}│${NC} ${white} Domain  ${NC}: $domain"
                     echo -e "${Kcyan}│${NC} ${white} CPU     ${NC}: $cpu_usage"  
                     echo -e "${Kcyan}│${NC} ${white} RAM     ${NC}: $tram Mb"
                     echo -e "${Kcyan}│${NC} ${white} VERSION ${NC}: 1.0 last update"
