51fdbfcd311ccb9108591c7cf583d9e1033b3ddd
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-25 21:55:58 +0700

    menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -11,7 +11,7 @@ clear
         white="\e[1;37m"
 clear        
 echo -e "╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕${NC}" | lolcat
-		    echo -e "║ \E[37;1;36m      👉 👑 ASSU TUNNEL 👑 👈           \E[0m"
+		    echo -e "║ \E[37;1;36m       👑 ALVI CELL TUNNEL 👑            \E[0m"
 		    echo -e "╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛${NC}" | lolcat 
 echo -e "  ${red_background}SYSTEM MENU${NC}"
 echo -e " "
