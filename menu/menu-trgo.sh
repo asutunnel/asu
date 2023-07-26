acfe8a922655c9392d7619a08ff3cbef732c8f57
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-26 23:17:37 +0700

    menu-trgo.sh

--- menu/menu-trgo.sh
+++ menu/menu-trgo.sh
@@ -81,9 +81,9 @@ exit 0
 fi
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear 
-echo -e "╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕${NC}" | lolcat
+echo -e "╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕${NC}"
 		    echo -e "║ \E[37;1;36m       👑 ALVI CELL TUNNEL 👑            \E[0m"
-		    echo -e "╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛${NC}" | lolcat 
+		    echo -e "╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛${NC}"
 echo -e   "${red_background}TROJAN-GO MENU${NC}"
 echo -e " "
 echo -e "         ${white}[•1]${NC} ${cyan}Create TrojanGo${NC}"
