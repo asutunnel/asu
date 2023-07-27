959caf3d0de215a2f4050c167be5af8e2a859ff2
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-28 06:09:43 +0700

    menu-vmess.sh

--- menu/menu-vmess.sh
+++ menu/menu-vmess.sh
@@ -35,15 +35,16 @@ if [[ $date_list < $useexp ]]; then
 echo -ne
 else
 echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-echo -e "\033[42m          ALVI CELL AUTOSCRIPT          \033[0m"
+echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
 echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
 echo -e ""
 echo -e "            ${RED}PERMISSION DENIED !${NC}"
 echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
 echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
 echo -e "             \033[0;33mContact Admin :${NC}"
-echo -e "      \033[0;36mTelegram${NC} t.me/Alvi_cell"
-echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282183496832""
+echo -e "      \033[0;36mTelegram${NC} t.me/Dragon_Emperor999"
+echo -e "      ${GREEN}WhatsApp${NC} wa.me/6283821682527"
+echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
 exit
 fi
 }
@@ -78,7 +79,7 @@ read -rp "Input Username : " user
 if [ -z $user ]; then
 menu
 else
-read -p "Limit Quota: Quota"
+read -p "Limit (Quota): " Quota
 echo -e "$[$Quota * 1024 * 1024 * 1024]" > /etc/vmess/${user}
 clear
 echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -121,7 +122,7 @@ read -rp "Input Username : " user
 if [ -z $user ]; then
 menu
 else
-read -p "Limit IP: " ips
+read -p "Limit (IP): " ips
 echo -e "${ips}" > /etc/vmess/limit-ip/${user}
 clear
 echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -198,9 +199,8 @@ echo -e " \033[35m│$NC Press x or [ Ctrl+C ] • To-Exit"
 echo -e " \033[36m╰══════════════════════════════════════════════════════════╯\e[0m"
 echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
 echo -e ""
-read -p " ====>>>  "  opt
-echo -e ""
-case $opt in
+read -p " Select From Options [ 1 - 8 ] : " menu
+case $menu in
 1) clear ; addws ; exit ;;
 2) clear ; trialvmess ; exit ;;
 3) clear ; del-ws ; exit ;;
@@ -211,4 +211,5 @@ case $opt in
 8) clear ; resquota ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
+*) echo "Anda salah tekan " ; sleep 1 ; menu ;;
 esac
