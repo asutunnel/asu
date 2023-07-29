c539a90d0f68a1e3b9d53f7ac4a76d8daeec8e85
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-29 23:26:40 +0700

    menu-bot.sh

--- menu/menu-bot.sh
+++ menu/menu-bot.sh
@@ -26,7 +26,7 @@ echo -e ""
 read -p " Select From Options [ 1 - 4 ] : " opt
 echo -e ""
 case $opt in
-1) clear ; wget https://raw.githubusercontent.com/asutunnel/asu/main/Panel/xolpanel.sh && chmod +x xolpanel.sh && ./xolpanel.sh ;;
+1) clear ; wget https://raw.githubusercontent.com/asutunnel/asu/main/bot/xpanel.sh && chmod +x xpanel.sh && ./xpanel.sh ;;
 2) clear ; add-bot-notif ; exit ;;
 3) clear ; bot-cek-tr ; exit ;;
 4) clear ; bot-cek-vless ; exit ;;
