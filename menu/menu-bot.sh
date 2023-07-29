e7601f3c1932c217a1b93ee768d03a2a44982697
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-29 13:21:35 +0700

    menu-bot.sh

--- menu/menu-bot.sh
+++ menu/menu-bot.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
-ipsaya=$(wget -qO- ipinfo.io/ip)
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/asutunnel/permission/main/ipmini"
-checking_sc() {
-useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-if [[ $date_list < $useexp ]]; then
-echo -ne
-else
-echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-echo -e "\033[42m          ALVI CELL AUTOSCRIPT          \033[0m"
-echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-echo -e ""
-echo -e "            ${RED}PERMISSION DENIED !${NC}"
-echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-echo -e "             \033[0;33mContact Admin :${NC}"
-echo -e "      \033[0;36mTelegram${NC} t.me/Alvi_cell"
-echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282183496832"
-echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-exit
-fi
-}
-checking_sc
-BOT="https://raw.githubusercontent.com//asutunnel/asu/bot/main/xpanel.sh"
+MYIP=$(wget -qO- ipinfo.io/ip)
+green_background="\033[42;37m"
+NC="\033[0m"
+red_background="\033[41;37m"
+wh="\e[1;37m" # PUTIH
+cy="\e[1;36m" # CYAN
+ye="\e[1;33m" # KUNING
+clear 
 clear
 echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
 echo -e " \033[31m╭══════════════════════════════════════════════════════════╮\e[0m"
