5f31fefc13133f0d5640ba9532798e6d873ea721
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-30 09:17:45 +0700

    menu-bot.sh

--- menu/menu-bot.sh
+++ menu/menu-bot.sh
@@ -8,6 +8,10 @@ cy="\e[1;36m" # CYAN
 ye="\e[1;33m" # KUNING
 clear 
 clear
+echo -e "\033[1;93m──────────────────────────────────────────────────────────\033[0m"
+echo -e "\033[42m                               👑 ALVI CELL TUNNEL 👑          \033[0m"
+echo -e "\033[1;93m────────────────────────────────────────────────────────────\033[0m"
+echo -e ""
 echo -e " \033[31m╭══════════════════════════════════════════════════════════╮\e[0m"
 echo -e " \033[34m│$NC\033[33m                      DAFTAR MENU BOT                           $NC\033[34m│\e[0m"
 echo -e " \033[33m╰══════════════════════════════════════════════════════════╯\e[0m"
@@ -23,7 +27,7 @@ echo -e " \033[35m│$NC [0]${NC} \033[0;36m Exit${NC}"
 echo -e " \033[36m╰══════════════════════════════════════════════════════════╯\e[0m"
 
 echo -e ""
-read -p " Select From Options [ 1 - 4 ] : " opt
+read -p " silakan pilih menu bosku : " opt
 echo -e ""
 case $opt in
 1) clear ; wget https://raw.githubusercontent.com/asutunnel/asu/main/bot/xpanel.sh && chmod +x xpanel.sh && ./xpanel.sh ;;
