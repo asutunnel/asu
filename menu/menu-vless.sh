bdcdf84a458cc5daf6a62b9398d28ba203831999
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-07-31 18:16:12 +0700

    menu-vless.sh

--- menu/menu-vless.sh
+++ menu/menu-vless.sh
@@ -1,99 +1,215 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-NC="\e[0m"
-RED="\033[0;31m" 
 
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/asutunnel/permission/main/ipmini > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
 
-MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/asutunnel/permission/main/ipmini | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
-else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/asutunnel/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-red='\e[1;31m'
-green='\e[1;32m'
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
 NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+grenbo="\e[92;1m"
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+export CHATID="5862048483"
+export KEY="6307367714:AAG_Gae1CEjB-BDTYLzHSb89kbmLxzln7mk"
+export TIME="10"
+export URL="https://api.telegram.org/bot$KEY/sendMessage"
+clear
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Andyyuda/permission/main/ip"
+checking_sc() {
+useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+if [[ $date_list < $useexp ]]; then
 echo -ne
 else
-red "Permission Denied!"
-exit 0
+echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+echo -e ""
+echo -e "            ${RED}PERMISSION DENIED !${NC}"
+echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+echo -e "             \033[0;33mContact Admin :${NC}"
+echo -e "      \033[0;36mTelegram${NC} t.me/Dragon_Emperor999"
+echo -e "      ${GREEN}WhatsApp${NC} wa.me/6283821682527"
+echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+exit
 fi
-
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
+}
+checking_sc
 clear
-
-echo -e "╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕${NC}"
-		    echo -e "║ \E[37;1;36m       👑 ALVI CELL TUNNEL 👑            \E[0m"
-		    echo -e "╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛${NC}"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[40;1;37m       • MENU VLESS •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+function editquota(){
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/vless/.vless.db")
+if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "           Edit Quota Vless          \E[0m"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo "You have no existing clients!"
+echo ""
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+fi
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "           Edit Quota Vless          \E[0m"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+grep -E "^#& " "/etc/vless/.vless.db" | cut -d ' ' -f 2 | column -t | sort | uniq
+echo ""
+red "tap enter to go back"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -rp "Input Username : " user
+if [ -z $user ]; then
+menu
+else
+read -p "Limit (Quota): " Quota
+echo -e "$[$Quota * 1024 * 1024 * 1024]" > /etc/vless/${user}
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo " Vless Account Was Successfully Edited"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo " Client Name  : $user"
+echo " Quota Ready  : $Quota GB"
+echo ""
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+fi
+}
+function editlimit(){
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/vless/.vless.db")
+if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "           Edit Limit Vless          \E[0m"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo "You have no existing clients!"
+echo ""
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+fi
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "           Edit Limit Vless          \E[0m"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+grep -E "^#& " "/etc/vless/.vless.db" | cut -d ' ' -f 2 | column -t | sort | uniq
+echo ""
+red "tap enter to go back"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -rp "Input Username : " user
+if [ -z $user ]; then
+menu
+else
+read -p "Limit (IP): " ips
+echo -e "${ips}" > /etc/vless/limit-ip/${user}
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo " Vless Account Was Successfully Edited"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo " Client Name  : $user"
+echo " Limit IP Ready  : $ips IP"
+echo ""
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+fi
+}
+function resquota(){
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/vless/.vless.db")
+if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "           Resset Quota Vless          \E[0m"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo "You have no existing clients!"
+echo ""
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+fi
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "           Resset Quota Vless          \E[0m"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+grep -E "^#& " "/etc/vless/.vless.db" | cut -d ' ' -f 2 | column -t | sort | uniq
+echo ""
+red "tap enter to go back"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -rp "Input Username : " user
+if [ -z $user ]; then
+menu
+else
+echo "0" > /etc/limit/vless/${user}
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo " Vless Account Was Successfully Resset"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo " Client Name  : $user"
+echo " Successfully Resset Quota"
+echo ""
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+fi
+}
+clear
+echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
+echo -e " \033[31m╭══════════════════════════════════════════════════════════╮\e[0m"
+echo -e " \033[34m│$NC\033[33m                       MENU VLESS                         $NC\033[34m│\e[0m"
+echo -e " \033[33m╰══════════════════════════════════════════════════════════╯\e[0m"
+echo -e " \033[32m╭══════════════════════════════════════════════════════════╮\e[0m"
+echo -e " \033[35m│$NC [01]${NC} \033[0;36m Creating VLESS Account WS/GRPC${NC}"
+echo -e " \033[35m│$NC [02]${NC} \033[0;36m Trial VLESS Account WS/GRPC${NC}"
+echo -e " \033[35m│$NC [03]${NC} \033[0;36m Delete VLESS Account WS/GRPC${NC}"
+echo -e " \033[35m│$NC [04]${NC} \033[0;36m Renew VLESS Account${NC}"
+echo -e " \033[35m│$NC [05]${NC} \033[0;36m Check VLESS login Account${NC}"
+echo -e " \033[35m│$NC [06]${NC} \033[0;36m Edit Quota Account ${NC}"
+echo -e " \033[35m│$NC [07]${NC} \033[0;36m Edit Limit IP Account${NC}"
+echo -e " \033[35m│$NC [08]${NC} \033[0;36m Reset Quota Account${NC}"
+echo -e " \033[35m│$NC"
+echo -e " \033[35m│$NC [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e " \033[35m│$NC"
+echo -e " \033[35m│$NC Press x or [ Ctrl+C ] • To-Exit"
+echo -e " \033[36m╰══════════════════════════════════════════════════════════╯\e[0m"
+echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Buat Vless "
-echo -e " [\e[36m•2\e[0m] Buat Trial "
-echo -e " [\e[36m•3\e[0m] Ubah Akun Vless "
-echo -e " [\e[36m•4\e[0m] Hapus Akun Vless "
-echo -e " [\e[36m•5\e[0m] Cek User Login Vless "
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-read -p " Select =>  "  opt
-echo -e ""
-case $opt in
+read -p " Select From Options [ 1 - 7 ] : " menu
+case $menu in
 1) clear ; add-vless ; exit ;;
 2) clear ; trialvless ; exit ;;
-3) clear ; renew-vless ; exit ;;
-4) clear ; del-vless ; exit ;;
+3) clear ; del-vless ; exit ;;
+4) clear ; renew-vless ; exit ;;
 5) clear ; cek-vless ; exit ;;
+6) clear ; editquota ; exit ;;
+7) clear ; editlimit ; exit ;;
+8) clear ; resquota ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
-*) echo "Anda salah tekan " ; sleep 1 ; menu-vless ;;
+*) echo "Anda salah tekan " ; sleep 1 ; menu ;;
 esac
