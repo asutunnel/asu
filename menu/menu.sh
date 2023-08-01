569c61e02f0e48e18fc51130e6ed51d92f0e6e79
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-08-01 07:11:50 +0700

    menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -438,7 +438,7 @@ echo -e   ""
 case $opt in
 1) clear ; menu-sshh ;;
 2) clear ; menu-vmess ;;
-3) clear ; menu-vless ; exit ;;
+3) clear ; menu-vless ; 
 4) clear ; menu-trgo ;;
 5) clear ; menu-trojan  ;;
 6) clear ; passwd ;;
