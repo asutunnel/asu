b9f178a08231fed4705b25a38684fbf5fb1f9547
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-08-04 10:34:44 +0700

    add-bot-notif.sh

--- bot/add-bot-notif.sh
+++ bot/add-bot-notif.sh
@@ -10,31 +10,7 @@ GREEN='\033[0;32m'
 grenbo="\e[92;1m"
 YELL='\033[0;33m'
 BGX="\033[42m"
-# Getting
-ipsaya=$(wget -qO- ipinfo.io/ip)
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/asutunnel/permision/main/ipmini"
-checking_sc() {
-  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-  if [[ $date_list < $useexp ]]; then
-    echo -ne
-  else
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-echo -e "\033[42m          👑 ALVI CELL TUNNEL 👑         \033[0m"
-echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-echo -e ""
-echo -e "            ${RED}PERMISSION DENIED !${NC}"
-echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-echo -e "             \033[0;33mContact Admin :${NC}"
-echo -e "      \033[0;36mTelegram${NC} t.me/Alvi_cell"
-echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282183496832"
-echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit
-  fi
-}
-checking_sc
+
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " \e[1;97;101m          ADD BOT NOTIFIKASI          \e[0m"
