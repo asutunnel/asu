d949b64f6585b2e01bb985666fb5268701399927
asutunnel <140300677+asutunnel@users.noreply.github.com>
2023-08-04 11:00:11 +0700

    add-ws.sh

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -832,6 +832,6 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 echo -e "Expired On     : $exp" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo "" | tee -a /etc/log-create-user.log
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p "Press any key to back on menu script by ALVI CELL"
 
 menu
